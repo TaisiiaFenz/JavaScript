aea95a2902e7e8070f3dbbdc670b5eac874c1a69
Taisiia Fenz <tayafenz@gmail.com>
2020-10-06 17:56:07 +0300

    add push and 3 version of unshift

---
+++ Andersen intership/polyfills/push+unshift.js
@@ -0,0 +1,48 @@
+Array.prototype.push0 = function() {
+    for (let argument of arguments) {
+        this[this.length] = argument;
+    }
+    return this.length;
+}
+
+Array.prototype.unshift0 = function() {
+    let arr = [];
+    for (let argument of arguments) {
+        arr[arr.length] = argument;
+    }
+    this.forEach((elem) => arr[arr.length] = elem);
+    arr.forEach((elem, index) => this[index] = elem);
+
+    return this.length;
+}
+
+Array.prototype.unshift1 = function() {
+    for (let i = this.length - 1; i >= 0; i--) {
+        this[i + arguments.length] = this[i];
+        console.log("he");
+    }
+    let i = 0;
+    for (let argument of arguments) {
+        this[i] = argument;
+        i++;
+    }
+    return this.length;
+}
+
+Array.prototype.unshift2 = function(){
+    console.log(arguments);
+    Array.prototype.splice.call(arguments, 0, 0, 0, 0);
+    console.log(arguments);
+    console.log(this);
+    Array.prototype.splice.apply(this, arguments);
+    console.log(this);
+    return this.length;
+};
+
+const arr1 = [1, 2, 3];
+arr1.push0(4, 5, 6, 7);
+console.log(arr1);
+
+const arr2 = [1, 3, 5];
+arr2.unshift2(2, 4, 6, 8);
+console.log(arr2);
